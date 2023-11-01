60f83e254bb30ed50c407ba5339901388fb026be
patilacerda <>
2023-11-01 20:35:31 +0000

    Create drink dictionary

--- run.py
+++ run.py
@@ -1,3 +1,59 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Define drink recipes using dictionaries
+drink_recipes = {
+    'gin': {
+        'citric': 'Gin and Tonic',
+        'berry': 'Bramble',
+        'sour': 'Gimlet',
+        'spicy': 'Negroni',
+        'herbal': 'Martini',
+        'sweet': 'Tom Collins',
+        'dry': 'Martini',
+        'creamy': 'Ramos Gin Fizz',
+        'fizzy': 'Gin Fizz'
+    },
+    'vodka': {
+        'citric': 'Screwdriver',
+        'berry': 'Cosmopolitan',
+        'sour': 'Lemon Drop',
+        'spicy': 'Bloody Mary',
+        'herbal': 'Moscow Mule',
+        'sweet': 'Sex on the Beach',
+        'dry': 'Vodka Martini',
+        'creamy': 'White Russian',
+        'fizzy': 'Vodka Soda'
+    },
+    'whisky': {
+        'citric': 'Whiskey Sour',
+        'berry': 'Blackberry Whiskey Smash',
+        'sour': 'Sazerac',
+        'spicy': 'Hot Toddy',
+        'herbal': 'Mint Julep',
+        'sweet': 'Boulevardier',
+        'dry': 'Manhattan',
+        'creamy': 'Whiskey Cream Soda',
+        'fizzy': 'Whiskey Highball'
+    },
+    'rum': {
+        'citric': 'Daiquiri',
+        'berry': 'Mai Tai',
+        'sour': 'Zombie',
+        'spicy': 'Rum Punch',
+        'herbal': 'Rum Swizzle',
+        'sweet': 'Pina Colada',
+        'dry': 'El Presidente',
+        'creamy': 'Coquito',
+        'fizzy': 'Rum and Coke'
+    },
+    'tequila': {
+        'citric': 'Margarita',
+        'berry': 'Tequila Sunrise',
+        'sour': 'Paloma',
+        'spicy': 'Bloody Maria',
+        'herbal': 'Agave Old Fashioned',
+        'sweet': 'Tequila Mockingbird',
+        'dry': 'Tequila Gimlet',
+        'creamy': 'Batanga',
+        'fizzy': 'Tequila Soda'
+    },
+    'I\'m feeling lucky': None
+}

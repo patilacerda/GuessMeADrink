084a9dc99554751329112d4833eb83e5c435d4a6
patilacerda <>
2023-11-03 07:18:40 +0000

    Define function to verify user age

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 # Define drink recipes using dictionaries
 drink_recipes = {
     'gin': {
@@ -80,3 +82,36 @@ menu_art = """
                                 ████████████
                             ▓▓▓▓████████████▓▓██
 """
+
+
+def calculate_age(birth_date):
+    """
+    Calculate the age based on the birth date.
+    """
+    today = datetime.today()
+    age = today.year - birth_date.year - (
+            (today.month, today.day) < (birth_date.month, birth_date.day))
+    return age
+
+
+print("Welcome to Guess me a drink")
+print(menu_art)
+
+# Wait for the user to press Enter
+while input("Press Enter to start...") != "":
+    pass
+
+# Verify the age of the user
+while True:
+    user_age_str = input("Please enter your date of birth (dd/mm/yyyy): ")
+    try:
+        user_age_date = datetime.strptime(user_age_str, "%d/%m/%Y")
+        user_age = calculate_age(user_age_date)
+        break
+    except ValueError:
+        print("Invalid date format. Please use the format dd/mm/yyyy.")
+
+if user_age < 18:
+    print("You can drink a soda buddy ^-^")
+else:
+    print("Let's get started!")
